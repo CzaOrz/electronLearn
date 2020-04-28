5337bd34b2bf365e8aa7ed19a625a00be4822bdc
cza <czasg0.0>
2020-04-28 11:00:27 +0800

    update

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 // require('./series/1_文件操作相关/index');
 // require('./series/2_主渲交互/index');
-require('./series/3_系统文件框/index');
+// require('./series/3_系统文件框/index');
+require('./series/4_菜单模块/index');
---
+++ series/4_菜单模块/index.js
@@ -0,0 +1,131 @@
+var
+    path = require('path'),
+    {app, BrowserWindow, Menu} = require('electron');
+
+if (!app.requestSingleInstanceLock()) app.quit();
+
+app.on('ready', () => {
+    var win = new BrowserWindow({
+        height: 800,
+        width: 1200,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        // title: '菜单模块',  // 当页面没有title的时候，可以在此处添加。但渲染进程中的优先级高一些
+        // icon: 'icon.png'  // 可以指定 图标
+    });
+
+    win.loadFile(path.join(__dirname, 'index.html'));
+
+    const isMac = process.platform === 'darwin';
+    const template = [
+        // { role: 'appMenu' }
+        ...(isMac ? [{
+            label: app.name,
+            submenu: [
+                {
+                    label: '陈子昂',
+                    role: 'about'
+                },
+                {type: 'separator'},
+                {role: 'services'},
+                {type: 'separator'},
+                {role: 'hide'},
+                {role: 'hideothers'},
+                {role: 'unhide'},
+                {type: 'separator'},
+                {role: 'quit'}
+            ]
+        }] : []),
+        // { role: 'fileMenu' }
+        {
+            label: 'File',
+            submenu: [
+                isMac ? {role: 'close'} : {role: 'quit'}
+            ]
+        },
+        // { role: 'editMenu' }
+        {
+            label: 'Edit',
+            submenu: [
+                {
+                    label: '陈子昂',
+                    role: 'undo'
+                },
+                {role: 'redo'},
+                {type: 'separator'},
+                {role: 'cut'},
+                {role: 'copy'},
+                {role: 'paste'},
+                ...(isMac ? [
+                    {role: 'pasteAndMatchStyle'},
+                    {role: 'delete'},
+                    {role: 'selectAll'},
+                    {type: 'separator'},
+                    {
+                        label: 'Speech',
+                        submenu: [
+                            {role: 'startspeaking'},
+                            {role: 'stopspeaking'}
+                        ]
+                    }
+                ] : [
+                    {role: 'delete'},
+                    {type: 'separator'},
+                    {role: 'selectAll'}
+                ])
+            ]
+        },
+        // { role: 'viewMenu' }
+        {
+            label: 'ViewCza',
+            submenu: [
+                {role: 'reload'},
+                {role: 'forcereload'},
+                {role: 'toggledevtools'},
+                {type: 'separator'},
+                {role: 'resetzoom'},
+                {role: 'zoomin'},
+                {role: 'zoomout'},
+                {type: 'separator'},
+                {role: 'togglefullscreen'}
+            ]
+        },
+        // { role: 'windowMenu' }
+        {
+            label: 'Window',
+            submenu: [
+                {role: 'minimize'},
+                {role: 'zoom'},
+                ...(isMac ? [
+                    {type: 'separator'},
+                    {role: 'front'},
+                    {type: 'separator'},
+                    {role: 'window'}
+                ] : [
+                    {role: 'close'}
+                ])
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Learn More',
+                    click: async () => {
+                        const {shell} = require('electron');
+                        await shell.openExternal('https://electronjs.org')
+                    }
+                }
+            ]
+        }
+    ];
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+});
+
