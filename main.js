1d84a8d4b83a5fc776a61be3d127fccaea4ba427
cza <czasg0.0>
2020-04-30 18:02:00 +0800

    update

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+/*
+* Project
+* */
+require('./project/1_打码器/index');
+
+/*
+* Learning Series
+* */
 // require('./series/1_文件操作相关/index');
 // require('./series/2_主渲交互/index');
 // require('./series/3_系统文件框/index');
 // require('./series/4_菜单模块/index');
 // require('./series/5_系统托盘/index');
-require('./series/6_session/index');
+// require('./series/6_session/index');
