a09f8b03732ee8d7c858f1c91a266bacfa13f324
czaorz <czasg0.0>
2020-05-05 21:15:31 +0800

    update

--- main.js
+++ main.js
@@ -3,4 +3,6 @@
 // require('./series/3_系统文件框/index');
 // require('./series/4_菜单模块/index');
 // require('./series/5_系统托盘/index');
-require('./series/6_session/index');
+// require('./series/6_session/index');
+
+require('./project/2_分类器/index');
